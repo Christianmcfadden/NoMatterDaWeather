fff77208de5468b543c40dbcfd0d887196f7e946
Damian Valdiviezo <d4valdiviezo@gmail.com>
2024-12-16 13:37:44 -0700

    commit changes in NMDW

---
+++ NMDW/weather_data.py
@@ -0,0 +1,10 @@
+# use for API interaction
+# data retrieval 
+
+import requests
+
+def get_weather(city):
+    #api_key =
+    #url = 
+    response = requests.get(url)
+    return response.json() 
