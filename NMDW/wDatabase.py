d71972a79b966c77fec78bb661a16fadefbb39ad
Christian L. Mcfadden <99759645+Christianmcfadden@users.noreply.github.com>
2024-12-21 15:41:14 -0500

    Update wDatabase.py

--- NMDW/wDatabase.py
+++ NMDW/wDatabase.py
@@ -1,7 +1,16 @@
-import sqlite3
+"""
+NoMatterDaWeather (Web App)
 
-def connect(db_name):
-    return sqlite3.connect(db_name)
+Contributers:
+Christian Mcfadden
+
+
+Date Started: 12/19/24
+
+File: wDatabase.py
+"""
+
+import sqlite3
 
 #The Weather App will store user info and 
 CREATE_USERS_TABLE = """CREATE TABLE users (
@@ -27,18 +36,22 @@ INSERT_USER = """INSERT INTO users (
         VALUES (?, ?, ?, ?, ?, ?, ?, ?);"""
 
 GET_ALL_USERS = "SELECT * FROM users;"
-GET_USERS_BY_NAME = "SELECT * FROM users WHERE fname = ?;"
+GET_USERS_BY_NAME = "SELECT * FROM users WHERE fname = ? or lname = ?;"
 DELETE_USER_BY_ID = "DELETE FROM users WHERE userid = ?;"  
-GET_USER_BY_USERNAME = "SELECT * FROM users WHERE username = ?;"
 GET_USER_BY_EMAIL = "SELECT * FROM users WHERE email = ?;"
 
+# This function can be used the connect to the database 
+def connect(db_name):
+    return sqlite3.connect(db_name)
+
+# This will be the function that you call on the first time starting the database
 def create_tables(connection):
     with connection:
         connection.execute(CREATE_USERS_TABLE)
 
-def add_user(connection, fname, lname, username, dob, age, sex, sexPref, email, password, city, zip, survey, match, bio, profile_pic):
+def add_user(connection, fname, lname, dob, email, password, city, zip, profile_pic):
     with connection:
-        connection.execute(INSERT_USER, (fname, lname, username, dob, age, sex, sexPref, email, password, city, zip, survey, match, bio, profile_pic))
+        connection.execute(INSERT_USER, (fname, lname, dob, email, password, city, zip, profile_pic))
 
 def get_all_users(connection):
     with connection:
