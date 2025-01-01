1ee7d3c4dc2771b3bffbdba2d92a95169fc1f222
Christian L. Mcfadden <99759645+Christianmcfadden@users.noreply.github.com>
2025-01-01 11:30:53 -0500

    Update forms.py
    Update 1/1/25

--- NMDW/forms.py
+++ NMDW/forms.py
@@ -10,8 +10,9 @@ Date Started: 12/7/24
 File: Forms.py
 """
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Regexp, ValidationError
+from flask_login import LoginManager, current_user
 from NMDW.models import User
 
 
@@ -20,15 +21,44 @@ class UserInfoForm(FlaskForm):
     fname = StringField('First Name', validators=[DataRequired()])
     lname = StringField('Last Name', validators=[DataRequired()])
     email = StringField('Email', validators=[DataRequired()])
-
-    password = StringField("password", validators=[DataRequired(),Regexp(
+    # add zip
+    
+    password = PasswordField("password", validators=[DataRequired(),Regexp(
                 r'^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$',
                 message="Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character, and must be at least 8 characters long."
             )])
-    
-    conpassword = StringField("Confirm Password", validators=[DataRequired()])
+    conpassword = PasswordField("Confirm Password", validators=[DataRequired()])
     submit = SubmitField('Next')
 
-    def validate_password(self):
-        if self.password.data != self.conpassword.data:
+    def validate_conpassword(self, conpassword):
+        if self.password.data != conpassword.data:
             raise ValidationError("Passwords must match.")
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username = username.data).first()
+        if user:
+            raise ValidationError("This username is already taken.")
+
+class UpdatedProfileForm(FlaskForm):
+    username = StringField('Username', validators = [DataRequired()])
+    fname = StringField('First Name', validators=[DataRequired()])
+    lname = StringField('Last Name', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired()])
+    zip_code = StringField('Zip Code', validators = [Regexp(r'^\d{5}(-\d{4})?$', message="Enter a valid ZIP code.")])
+    submit = SubmitField('Update Profile')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username = username.data).first()
+        if user:
+            raise ValidationError("This username is already taken.")
+
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError("This email is already registered.")
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired()])
+    password = StringField('Password', validators=[DataRequired()])
+    submit = SubmitField('Login')
