fe7f5eb4d1da77c3cbfb8661063cc34e35f43c7b
Christian L. Mcfadden <99759645+Christianmcfadden@users.noreply.github.com>
2025-01-01 11:10:02 -0500

    Update __init__.py
    Pull Merge 1/1/25 Happy New Year

--- NMDW/__init__.py
+++ NMDW/__init__.py
@@ -28,7 +28,6 @@ app.debug = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 db = SQLAlchemy(app)
 
-# i might use firebase to handle login
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = "login"
@@ -40,51 +39,4 @@ bcrypt = Bcrypt(app)
 
 from NMDW import routes
 
-"""
-How To refreeze the requirements
 
-    * Freeze Any New Requirements:
-        (pip freeze > requirements.txt)
-
-    * Mention in request to submit that you need the requirements updated Note the packages added
-
-Clone the GitHub Repository
-    * Clone the Repositiory:
-        (git clone https://github.com/Christianmcfadden/NoMatterDaWeather.git)
-
-    * Navigate to Repositiory:
-        (cd my-repo)
-
-How To Stage The File
-    (git add .)   <-- Adds modified files to the staging area
-    (git add file1.txt file2.txt) <-- If you manually want to stage specific files
-
-    
-How To Commit Files To The Main Branch In The Repository
-    * Stage The Files:
-        
-    * Commit The Files:
-        Commit Files With Message:
-            (git commit -m "Your commit message")
-        Push To Main:
-            (git push origin main)
-
-
-How To Commit Files To A New Branch In The Repository
-    * Stage The Files:
-
-    * Commit The Files:
-        Create a New Branch:
-            (git checkout -b your-branch-name)
-        
-        Push the New Branch:
-            (git push origin your-branch-name)
-
-        Submit a Pull Request (PR):
-            Go to the original repository on GitHub.
-            You’ll see a prompt to create a pull request for the new branch.
-            Click Compare & pull request.
-            Add a description of your changes and submit the PR for review.
-
-
-"""
