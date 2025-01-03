5cd7d227f46702ff496eccbac0898d3d6d48b47e
Christian L. Mcfadden <99759645+Christianmcfadden@users.noreply.github.com>
2025-01-03 16:49:53 -0500

    Update models.py

--- NMDW/models.py
+++ NMDW/models.py
@@ -22,8 +22,7 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(60), nullable=False)
 
-    @login_manager.user_loadergit
-
+    @login_manager.user_loader
     def load_user(user_id):
         return User.query.get(int(user_id))
 
