decac761a8d41b7fb7f2095c7c7fe6cc044d2911
Christian L. Mcfadden <99759645+Christianmcfadden@users.noreply.github.com>
2025-01-04 13:41:56 -0500

    Update routes.py

--- NMDW/routes.py
+++ NMDW/routes.py
@@ -11,8 +11,8 @@ File: Routes.py
 """
 from flask import render_template, url_for, flash, redirect, session, request
 from NMDW import app, db, bcrypt
-from NMDW.forms import LoginForm, UserInfoForm, UserCredentialsForm, UpdateNameForm, UpdateUsernameForm, UpdateEmailForm, UpdateDobForm, UpdateGenderForm, UpdatePasswordForm, UpdateZipForm, UpdateBioForm, UpdateProfilePicForm, PostForm
-from NMDW.models import User, Post, Flag, Review, ActivityLog
+from NMDW.forms import LoginForm, UserInfoForm
+from NMDW.models import User
 from flask_login import login_user, current_user, logout_user, login_required
 from datetime import datetime, date
 import os
